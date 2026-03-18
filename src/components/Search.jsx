d7431c90a9be7a7607e16bd9ae7b661c81f32fbe
Armando <argradi2003@gmail.com>
2026-03-18 16:44:59 +0100

    Lab-React-Iron-Brewers

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,4 +1,27 @@
-function Search() {
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+function Search(props) {
+  const [search, setSearch] = useState("");
+  const handleSearch = (e) => setSearch(e.target.value);
+
+  const baseURL = 'https://beers-api.edu.ironhack.com/beers'
+
+  useEffect(() => {
+    getSearch()
+  }, [search])
+
+  const getSearch = () => {
+    axios
+      .get(baseURL + `/search?q=${search}`)
+      .then((beer) => {
+        props.onSet(beer.data)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
   return (
     <div className="d-inline-flex justify-content-center align-items-center w-100 p-4">
       <div className="input-group mb-2 w-50">
@@ -10,6 +33,8 @@ function Search() {
         <input
           type="text"
           className="form-control search-bar"
+          value={search}
+          onChange={handleSearch}
         />
       </div>
     </div>
